8202f4d847d6def7fcf50341919b2a2c06a48832
meggart <fabiangans@gmx.de>
2013-06-02 21:01:28 +0200

    step back

--- ncplot.jl
+++ ncplot.jl
@@ -12,12 +12,12 @@ function ncplot(fn::String)
         #Average over all time steps
         ntime=v.dim[3].dimlen
         a=NetCDF.readvar(nc,v,[1,1,1],[-1,-1,1])
-        if ntime>1
-          for i=2:ntime
-            a=a+NetCDF.readvar(nc,v,[1,1,i],[-1,-1,1])
-          end
-          a=(a./ntime)[:,:,1]
-        end
+        #if ntime>1
+        #  for i=2:ntime
+        #    a=a+NetCDF.readvar(nc,v,[1,1,i],[-1,-1,1])
+        #  end
+        #  a=(a./ntime)[:,:,1]
+        #end
       end
       missval = has(v.atts,"missing_value") ? v.atts["missing_value"] : 1.0e32
       imageterm(a,missval=missval)
